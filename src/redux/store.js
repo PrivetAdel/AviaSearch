da8ee6e97f53b1e2b7e6c1e0921bdf52c9f5a9f3
Adel <rizvanovaadel@gmail.com>
2021-05-04 21:42:46 +0300

    correct spelling mistake

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
-import rootReduser from './reducers/rootReduser';
+import rootReducer from './reducers/rootReducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  rootReduser,
+  rootReducer,
   composeEnhancers(applyMiddleware(thunk))
 );
 
