0d55c953372c80e31dc709598a2702fd05089d08
Adel <rizvanovaadel@gmail.com>
2021-06-01 13:44:29 +0300

    fix deploy

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./public/flights.json');
-const middlewares = jsonServer.defaults({
-  static: './build'
-});
+const express = require('express');
+ const path = require('path');
+ const PORT = process.env.PORT || 3000;
+ 
+ const app = express();
+ 
+ app.use(express.static(__dirname));
+ app.use(express.static(path.join(__dirname, 'build')));
+ 
+ app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
 
-const PORT = process.env.PORT || 3001;
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(PORT, () => {
+ app.listen(PORT, () => {
   console.log('Server is running');
 });
